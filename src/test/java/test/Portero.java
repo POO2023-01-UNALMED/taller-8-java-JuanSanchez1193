6c76ddcbe0a81e629cf1ab0fd93379a6d695f1f3
Juan Jose Pineda Sanchez <jpinedasa@unal.edu.co>
2023-05-22 20:42:37 -0500

    a
    a

---
+++ src/test/java/test/Portero.java
@@ -0,0 +1,38 @@
+package test;
+
+public class Portero extends Futbolista{
+	
+	
+	  public short golesRecibidos;
+	  public byte dorsal;
+	  
+	  
+	  public Portero(String nombre,int edad, short golesMarcados,byte dorsal){
+	    super(nombre,edad,"Portero");
+	    this.golesRecibidos=golesMarcados;
+	    this.dorsal=dorsal;
+	  }
+	  
+	  
+	  public int compareTo(Object p){
+	    if(golesRecibidos==((Portero)p).golesRecibidos){
+	      return 0;
+	    }
+	    
+	    
+	    else if(golesRecibidos<((Portero)p).golesRecibidos){
+	      return ((Portero)p).golesRecibidos-this.golesRecibidos;
+	    }
+	    else{
+	      return this.golesRecibidos-((Portero)p).golesRecibidos;
+	    }
+	  }
+
+	  @Override
+	  public String toString(){
+	    return "El futbolista "+getNombre()+" tiene "+getEdad()+", y juega de "+getPosicion()+" con el dorsal "+dorsal+". Le han marcado "+ golesRecibidos;
+	  }
+	  	  public boolean jugarConLasManos(){
+	    return true;
+	  }
+	}
