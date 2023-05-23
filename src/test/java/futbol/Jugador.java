a2ac4268af60a77bbcc5c93293dc5f6e0101dc73
Juan Jose Pineda Sanchez <jpinedasa@unal.edu.co>
2023-05-22 20:48:02 -0500

    a
    a

--- src/test/java/futbol/Jugador.java
+++ src/test/java/futbol/Jugador.java
@@ -1,4 +1,4 @@
-package test;
+package futbol;
 
 public class Jugador extends Futbolista {
 	  public short golesMarcados;
--- src/test/java/futbol/Portero.java
+++ src/test/java/futbol/Portero.java
@@ -1,4 +1,4 @@
-package test;
+package futbol;
 
 public class Portero extends Futbolista{
 	
