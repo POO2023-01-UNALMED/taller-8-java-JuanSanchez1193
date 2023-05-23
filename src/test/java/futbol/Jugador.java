f579bfd35a39fcd37e5eb618e0389adffb01b42d
Juan Jose Pineda Sanchez <jpinedasa@unal.edu.co>
2023-05-22 20:53:29 -0500

    aaaaaaaaaaaaaaaaaa
    aaaaaaaaaaa

--- src/test/java/futbol/Jugador.java
+++ src/test/java/futbol/Jugador.java
@@ -1,38 +1,35 @@
 package futbol;
 
-public class Jugador extends Futbolista {
-	  public short golesMarcados;
-	  public byte dorsal;
-	  
-	  public Jugador(String nombre, int edad,  String posicion, short golesMarcados, byte dorsal) {
-		  super(nombre,edad,posicion);
-		  this.golesMarcados = golesMarcados;
-		  this.dorsal = dorsal;
-	  }
-	  
-	  public Jugador() {
-		  super();
-		  this.golesMarcados = 289;
-		  this.dorsal = 7;
-	  }
-	  
-	  public int compareTo(Object f){
-		    if(this.getEdad()==((Futbolista) f).getEdad()){
-		      return 0;
-		    }
-		    else if(this.getEdad()<((Futbolista) f).getEdad()){
-		      return ((Futbolista) f).getEdad()-this.getEdad();
-		    }
-		    else{
-		      return this.getEdad()-((Futbolista) f).getEdad();
-		    }
-		  }
-	  @Override
-	  public String toString(){
-		    return "El futbolista "+getNombre()+" tiene "+getEdad()+", y juega de "+getPosicion()+" con el dorsal "+dorsal+". Ha marcado "+ golesMarcados;
-		  }
-	  
-	  public boolean jugarConLasManos(){
-		  return false;
-		}
+public class Jugador extends Futbolista{
+  public short golesMarcados;
+  public byte dorsal;
+  public Jugador(String nombre,int edad,String posicion, short golesMarcados,byte dorsal){
+    super(nombre,edad,posicion);
+    this.golesMarcados=golesMarcados;
+    this.dorsal=dorsal;
+  }
+  public Jugador(){
+    super();
+    this.golesMarcados=289;
+    this.dorsal=7;
+  }
+  public int compareTo(Object f){
+    if(this.getEdad()==((Futbolista)f).getEdad()){
+      return 0;
+    }
+    else if(this.getEdad()<((Futbolista)f).getEdad()){
+      return ((Futbolista)f).getEdad()-this.getEdad();
+    }
+    else{
+      return this.getEdad()-((Futbolista)f).getEdad();
+    }
+  }
+  public boolean jugarConLasManos(){
+    return false;
+  }
+  @Override
+  public String toString(){
+    return "El futbolista "+getNombre()+" tiene "+getEdad()+", y juega de "+getPosicion()+" con el dorsal "+dorsal+". Ha marcado "+ golesMarcados;
+  }
+  
 }
--- src/test/java/futbol/Portero.java
+++ src/test/java/futbol/Portero.java
@@ -1,25 +1,17 @@
 package futbol;
 
 public class Portero extends Futbolista{
-	
-	
 	  public short golesRecibidos;
 	  public byte dorsal;
-	  
-	  
 	  public Portero(String nombre,int edad, short golesMarcados,byte dorsal){
 	    super(nombre,edad,"Portero");
 	    this.golesRecibidos=golesMarcados;
 	    this.dorsal=dorsal;
 	  }
-	  
-	  
 	  public int compareTo(Object p){
 	    if(golesRecibidos==((Portero)p).golesRecibidos){
 	      return 0;
 	    }
-	    
-	    
 	    else if(golesRecibidos<((Portero)p).golesRecibidos){
 	      return ((Portero)p).golesRecibidos-this.golesRecibidos;
 	    }
@@ -27,12 +19,12 @@ public class Portero extends Futbolista{
 	      return this.golesRecibidos-((Portero)p).golesRecibidos;
 	    }
 	  }
-
+	  public boolean jugarConLasManos(){
+	    return true;
+	  }
 	  @Override
 	  public String toString(){
 	    return "El futbolista "+getNombre()+" tiene "+getEdad()+", y juega de "+getPosicion()+" con el dorsal "+dorsal+". Le han marcado "+ golesRecibidos;
 	  }
-	  	  public boolean jugarConLasManos(){
-	    return true;
-	  }
+	  
 	}
